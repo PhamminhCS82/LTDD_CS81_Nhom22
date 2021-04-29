7f8a53f7852510704ffe4b74bf225841b8bfd762
tatquangkiet789 <52742909+tatquangkiet789@users.noreply.github.com>
2021-04-29 10:53:19 +0700

    Xoa cai animation, them cai toolbar va cai menu ben phai

--- app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -1,35 +1,60 @@
 package com.example.weatherforecast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     LinearLayout addCity;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         addCity = findViewById(R.id.addCity);
 
-        addCity.setOnClickListener(this);
+        addCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
-    public void onClick(View v) {
-        if(v.getId() == R.id.addCity) {
-            Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-            startActivity(intent);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
-            overridePendingTransition(R.anim.slide_down, R.anim.slide_up);
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                Toast.makeText(MainActivity.this, "Ứng dụng dự bán thời tiết", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.contact:
+                Toast.makeText(MainActivity.this, "Liên hệ với chúng tôi", Toast.LENGTH_LONG).show();
+                return true;
         }
+        return super.onOptionsItemSelected(item);
     }
 }
