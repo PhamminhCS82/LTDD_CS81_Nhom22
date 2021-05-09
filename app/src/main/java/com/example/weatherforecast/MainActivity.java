a9b0bef08a0a538ab7d81c45d213b64e34a8990b
tatquangkiet789 <52742909+tatquangkiet789@users.noreply.github.com>
2021-05-09 21:27:54 +0700

    Them chuc nang sinh ra layout

--- app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -1,24 +1,31 @@
 package com.example.weatherforecast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    LinearLayout addCity;
+    LinearLayout addCity, layoutList;
     Toolbar toolbar;
+    Button addLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
 
         addCity = findViewById(R.id.addCity);
         toolbar = findViewById(R.id.toolBar);
+        
+        addLayout = findViewById(R.id.add);
+        layoutList = findViewById(R.id.layout_container);
+
         setSupportActionBar(toolbar);
 
         addCity.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +47,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        addLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v = getLayoutInflater().inflate(R.layout.layout_add_city, null);
+
+                TextView nhietDo = (TextView) v.findViewById(R.id.nhietDo);
+                TextView thanhPho = (TextView) v.findViewById(R.id.thanhPho);
+                ImageView iconThoiTiet = (ImageView) v.findViewById(R.id.iconThoiTiet);
+
+                nhietDo.setText("160°");
+                thanhPho.setText("Hà Nội");
+                iconThoiTiet.setImageResource(R.drawable.rain);
+
+                layoutList.addView(v);
+            }
+        });
+
     }
 
     @Override
@@ -49,12 +78,12 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.about:
-                Toast.makeText(MainActivity.this, "Thông tin ứng dụng", Toast.LENGTH_LONG).show();
+                Intent aboutActivity = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(aboutActivity);
                 return true;
             case R.id.contact:
-                Toast.makeText(MainActivity.this, "Liên hệ với chúng tôi", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(MainActivity.this, ContactActivity.class);
-                startActivity(intent);
+                Intent contactActivity = new Intent(MainActivity.this, ContactActivity.class);
+                startActivity(contactActivity);
                 return true;
         }
         return super.onOptionsItemSelected(item);
